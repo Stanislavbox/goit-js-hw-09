bd6aa4318843cc5fdfca37bd6a9c6147ebca15d0
Utilizator <Stanislavbox24@gmail.com>
2023-04-14 05:37:47 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+stopBtn.disabled = true;
+
+startBtn.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      timerId = setInterval(() => {
+            startBtn.disabled = true;
+            stopBtn.disabled = false;
+            document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+});
+
+
+stopBtn.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      clearInterval(timerId);
+      startBtn.disabled = false;
+      stopBtn.disabled = true;
+});
